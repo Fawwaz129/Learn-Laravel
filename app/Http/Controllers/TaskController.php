661a8dd816eeca7d49bd10e3b8579820e24f5a9b
Fawwaz129 <wazwazzsrhrr@gmail.com>
2023-02-03 11:03:36 +0700

    akses middleware for admin

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -11,6 +11,7 @@ class TaskController extends Controller
 {
     public function __construct(){
         $this->middleware('auth');
+        //$this->middleware('is_admin');
     }
 
     public function index(Request $request) {
